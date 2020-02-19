54d3b07fb9aed30542f241e04366be5aa42686cc
Fernando Alfredo Sánchez Valadez <fernandoa.sanchez@softtek.com>
2020-02-19 09:51:24 -0600

    first commit

---
+++ WebApi/Controllers/WebApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        Models.Team4RMEntities BD = new Models.Team4RMEntities();
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody]Models.Log TYT)
+        {
+            using (Models.Team4RMEntities BD = new Models.Team4RMEntities())
+            {
+                BD.Log.Add(TYT);
+                BD.SaveChanges();
+            }
+            return Request.CreateResponse(HttpStatusCode.Created, TYT);
+        }
+    }
+}
---
+++ WebApi/Models/DataLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class DataLog
+    {
+        public int id { get; set; }
+        public string aplicacion { get; set; }
+        public string mensaje { get; set; }
+        public System.DateTime fecha { get; set; }
+    }
+}
